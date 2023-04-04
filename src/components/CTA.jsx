e5d23829362b7d7260c78e575c79d7723ec83131
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-05 10:26:41 +1200

    CTA section completed for mobile

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const CTA = () => {
   return (
     <>
-      <section className='cta-section'>
-        
+      <section className="cta-section">
+        <img src="/images/illustration-intro.png" alt="" />
+        <h1 className="cta-header">
+          All your files in one secure location, accessible anywhere.
+        </h1>
+        <p className="cta-details">
+          Fylo stores all your most important files in one secure location.
+          Access them wherever you need, share and collaborate with friends
+          family, and co-workers.
+        </p>
+        <div className="cta-btn-section">
+          <button className="cta-btn">
+            <a className="btn-text" href="#">Get Started</a>
+          </button>
+        </div >
       </section>
     </>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
