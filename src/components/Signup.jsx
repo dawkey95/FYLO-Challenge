6f728a48e2079c0b679c9fbde36c5035ab55f98e
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-06 10:59:34 +1200

    Signup text spacing and font size

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,16 +3,24 @@ const Signup = () => {
     <>
       <section className="signup-section">
         <div className="signup-container">
-          <h2 className="signup-title">Get early access today</h2>
-          <p className="signup-details">
-            It only takes a minute to sign up and our free starter tier is
-            extremely generous. If you have any questions, our support team
-            would be happy to help you.
-          </p>
-          <div className="btn-group">
-            <input className="signup-input" type="text" placeholder="email@example.com" />
-            <button className="signup-btn">Get Started For Free</button>
+          <div className="signup-text">
+            <h2 className="signup-title">Get early access today</h2>
+            <p className="signup-details">
+              It only takes a minute to sign up and our free starter tier is
+              extremely generous. If you have any questions, our support team
+              would be happy to help you.
+            </p>
           </div>
+          <form onSubmit={""} className="btn-group">
+              <input
+                className="signup-input"
+                type="text"
+                placeholder="email@example.com"
+              />
+            <button className="signup-btn" type="submit">
+              Get Started For Free
+            </button>
+          </form>
         </div>
       </section>
     </>
