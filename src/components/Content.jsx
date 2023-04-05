546b2e18157152036b90ff2f1dec77d14f9d37bd
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-05 22:02:13 +1200

    Finished Content Section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ const Content = () => {
     <>
       <section className="content-section">
         <img
-          src="/public/images/illustration-stay-productive.png"
+          src="/images/illustration-stay-productive.png"
           alt=""
           className="content-img"
         />
@@ -12,15 +12,17 @@ const Content = () => {
           <p className="content-p">
             Never let location be an issue when accessing your files. Fylo has
             you covered for all of your file storage needs.
-          </p>
-          <br />
-          <p className="content-p">
+            <br />
+            <br />
             Securely share files and folders with friends, family and colleagues
             for live collaboration. No email attachments required.
           </p>
+          <br />
+          <p className="content-p"></p>
         </div>
         <button className="content-btn">
           <a href="#">See how Fylo works</a>
+          <img src="/images/icon-arrow.svg" alt="arrow point right" />
         </button>
       </section>
     </>
