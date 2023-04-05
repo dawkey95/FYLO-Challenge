12fe3a6ec37d8c11dacd5a44a8653f5a8cc0543c
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-06 09:34:15 +1200

    Footer compelted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,59 +3,61 @@ const Footer = () => {
     <>
       <section className="footer">
         <img className="footer-logo" src="/images/logo.svg" alt="" />
-        <div className="footer-contact-details">
-          <div className="footer-contact-address">
-            <img
-              className="address-icon"
-              src="/images/icon-location.svg"
-              alt=""
-            />
-            <p className="address-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </p>
-          </div>
-
-          <div className="footer-contact-phone">
-            <img className="phone-icon" src="/images/icon-phone.svg" alt="" />
-            <p className="phone-text">+1-543-123-4567</p>
-          </div>
-
-          <div className="footer-contact-email">
-            <img className="email-icon" src="/images/icon-email.svg" alt="" />
-            <p className="email-text">example@fylo.com</p>
-          </div>
-        </div>
-
-        <div className="group1">
-          <span>About Us</span>
-          <span>Jobs</span>
-          <span>Press</span>
-          <span>Blog</span>
-        </div>
-
-        <div className="group2">
-          <span>Contact Us</span>
-          <span>Terms</span>
-          <span>Privacy</span>
-        </div>
-
-        <div className="social-icons">
-          <div className="circle-container">
-            <div className="circle-radius">
-              <i class="fa-brands fa-facebook-f" />
+        <div className="footer-container">
+          <div className="footer-contact-details">
+            <div className="footer-contact-address">
+              <img
+                className="address-icon"
+                src="/images/icon-location.svg"
+                alt=""
+              />
+              <p className="address-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
+            </div>
+  
+            <div className="footer-contact-phone">
+              <img className="phone-icon" src="/images/icon-phone.svg" alt="" />
+              <p className="phone-text">+1-543-123-4567</p>
+            </div>
+  
+            <div className="footer-contact-email">
+              <img className="email-icon" src="/images/icon-email.svg" alt="" />
+              <p className="email-text">example@fylo.com</p>
             </div>
           </div>
-
-          <div className="circle-container">
-            <div className="circle-radius">
-              <i class="fa-brands fa-twitter" />
-            </div>
+  
+          <div className="group1">
+            <span>About Us</span>
+            <span>Jobs</span>
+            <span>Press</span>
+            <span>Blog</span>
           </div>
-
-          <div className="circle-container">
-            <div className="circle-radius">
-              <i class="fa-brands fa-instagram" />
+  
+          <div className="group2">
+            <span>Contact Us</span>
+            <span>Terms</span>
+            <span>Privacy</span>
+          </div>
+  
+          <div className="social-icons">
+            <div className="circle-container">
+              <div className="circle-radius">
+                <i class="fa-brands fa-facebook-f" />
+              </div>
+            </div>
+  
+            <div className="circle-container">
+              <div className="circle-radius">
+                <i class="fa-brands fa-twitter" />
+              </div>
+            </div>
+  
+            <div className="circle-container">
+              <div className="circle-radius">
+                <i class="fa-brands fa-instagram" />
+              </div>
             </div>
           </div>
         </div>
