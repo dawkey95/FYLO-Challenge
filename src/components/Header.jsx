3f641f9c3a31f57cd147f8d8c1232b5bfc36febd
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-07 09:25:09 +1200

    btns roudned-full added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,17 @@ const Header = () => {
         <nav className="header-nav">
           <ul className="header-nav-list">
             <li className="header-nav-list-item">
-              <a href="#" className="header-nav-list-item-link">
+              <a href="#" className="header-features">
                 Features
               </a>
             </li>
             <li className="header-nav-list-item">
-              <a href="#" className="header-nav-list-item-link">
+              <a href="#" className="header-team">
                 Team
               </a>
             </li>
             <li className="header-nav-list-item">
-              <a href="#" className="header-nav-list-item-link">
+              <a href="#" className="header-signin">
                 Sign In
               </a>
             </li>
